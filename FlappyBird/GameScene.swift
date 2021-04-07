609e8a702d3dcad6e5c2426d750c5763941d88f4
Wataru.Nakamura <puipui.sentinel@gmail.com>
2021-04-07 16:04:21 +0900

    ちくわ登場

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -11,13 +11,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var scrollNode:SKNode!
     var wallNode:SKNode!
+    var chikuwaNode:SKNode!
     var bird:SKSpriteNode!
+    var chikuwa:SKSpriteNode!
     
     // 衝突判定カテゴリー ↓追加
     let birdCategory: UInt32 = 1 << 0       // 0...00001
     let groundCategory: UInt32 = 1 << 1     // 0...00010
     let wallCategory: UInt32 = 1 << 2       // 0...00100
     let scoreCategory: UInt32 = 1 << 3      // 0...01000
+    let chikuwacategory: UInt32 = 1 << 4
 
     
     // スコア用
@@ -41,14 +44,59 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scrollNode = SKNode()
         addChild(scrollNode)
         
-        wallNode = SKNode()   // 追加
-        scrollNode.addChild(wallNode)   // 追加
+        chikuwaNode = SKNode()
+        wallNode = SKNode()// 追加
+        scrollNode.addChild(wallNode)// 追加
+        scrollNode.addChild(chikuwaNode)
         
         setupGround()
         setupCloud()
         setupWall()
         setupBird()
         setupScoreLabel()
+        setupChikuwa()
+    }
+    
+    func setupChikuwa(){
+        let chikuwaTexture = SKTexture(imageNamed: "chikuwa")
+        
+        chikuwaTexture.filteringMode = .linear
+        
+        let movingDistance = CGFloat(self.frame.size.width + chikuwaTexture.size().width)
+        
+        let moveChikuwa = SKAction.moveBy(x: -movingDistance, y: 0, duration: 4)
+        
+        let removeChikuwa = SKAction.removeFromParent()
+        
+        let repeatChikuwaAnimation = SKAction.repeatForever(SKAction.sequence([moveChikuwa, removeChikuwa]))
+        
+        let birdSize = SKTexture(imageNamed: "bird_a").size()
+
+        let random_y_range = birdSize.height * 3.5
+
+        let slit_length = birdSize.height * 3
+        let groundSize = SKTexture(imageNamed: "ground").size()
+        let under_y = groundSize.height + (self.frame.size.height - groundSize.height) / 2 - slit_length / 2
+
+        let Chikuwacreate = SKAction.run({
+            let Chikuwa = SKSpriteNode(texture: chikuwaTexture)
+            
+            let random_y = CGFloat.random(in: 0..<random_y_range)
+            let under_chikuwa_y = under_y + random_y
+            
+            Chikuwa.position = CGPoint(x: self.frame.size.width + chikuwaTexture.size().width / 2, y: under_chikuwa_y )
+            Chikuwa.zPosition = 50
+            Chikuwa.size = CGSize(width: Chikuwa.size.width*0.1, height: Chikuwa.size.height*0.1)
+        
+            Chikuwa.run(repeatChikuwaAnimation)
+            self.chikuwaNode.addChild(Chikuwa)
+        }
+        )
+        let waitChikuwa = SKAction.wait(forDuration: 2)
+        
+        let repeatForeverChikuwaAnimation = SKAction.repeatForever(SKAction.sequence([Chikuwacreate , waitChikuwa]))
+
+        chikuwaNode.run(repeatForeverChikuwaAnimation)
     }
     
     
